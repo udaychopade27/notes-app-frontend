2671d8d2e93a63ed020acb47785ab73d48e780ec
Uday Hari Chopade <133924961+udaychopade27@users.noreply.github.com>
2026-03-10 15:21:31 +0530

    Update API endpoints to include '/api' prefix

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
-export const getNotes = () => API.get("/notes");
+export const getNotes = () => API.get("/api/notes");
 
 export const addNote = (text) =>
-  API.post("/notes", { text });
+  API.post("/api/notes", { text });
